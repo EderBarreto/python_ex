553125373fad32590e1a1ac7f74cd940ebf2efee
Éderson Barreto <edersonguimaraes@hotmail.com>
2022-09-30 15:18:56 -0300

    Dicionários

--- 01_acessando_dados.py
+++ 01_acessando_dados.py
@@ -1,5 +1,11 @@
-numeros = {1, 2, 3, 2}
+dados = {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
 
-numeros = list(numeros)
+print(dados["nome"])  # "Guilherme"
+print(dados["idade"])  # 28
+print(dados["telefone"])  # "3333-1234"
 
-print(numeros[0])
+dados["nome"] = "Maria"
+dados["idade"] = 18
+dados["telefone"] = "9988-1781"
+
+print(dados)  # {"nome": "Maria", "idade": 18, "telefone": "9988-1781"}
